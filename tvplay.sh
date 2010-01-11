6106f8c47a82896903c9ac55519561ad2bbd2c19
Gerard Lledo <gerard.lledo@gmail.com>
2010-01-11 16:50:35 +0200

    Fix for files that contain ' in the name

--- tvplay.sh
+++ tvplay.sh
@@ -4,6 +4,10 @@ TVPLAY_XINITRC=$HOME/.tvplay.xinitrc
 PLAYER=mplayer
 OPTS="-fs"
 
-echo "xterm -e \"$PLAYER $OPTS '$*'\"" > $TVPLAY_XINITRC
+# Prepend 2 backslashes to ' so it the escaping persists trought the bash
+# escape removal.
+VIDS=`echo "$*" | sed "s/'/\\\\\'/g"`
+
+echo "xterm -e \"$PLAYER $OPTS $VIDS\"" > $TVPLAY_XINITRC
 XINITRC=$TVPLAY_XINITRC xinit
 rm $TVPLAY_XINITRC
