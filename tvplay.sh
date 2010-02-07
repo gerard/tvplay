92eaee666bb7ef6e0cfb5bc576c2e5000ab3980a
Gerard Lledo <gerard.lledo@gmail.com>
2010-02-07 13:39:29 +0200

    Fix files with special characters
    Third commit trying to fix the same thing, hopefully this works for all
    cases.

--- tvplay.sh
+++ tvplay.sh
@@ -4,10 +4,23 @@ TVPLAY_XINITRC=$HOME/.tvplay.xinitrc
 PLAYER=mplayer
 OPTS="-fs"
 
-# Prepend 2 backslashes to ' so it the escaping persists trought the bash
-# escape removal.
-VIDS=`echo "$*" | sed "s/'/\\\\\'/g"`
+# Since using filenames with special characters is a PITA, we just link the
+# the file instead using a symbolic link.  That way we don't have to deal with
+# special characters when writing the xinitrc file.
+# Links will be called 1.avi, 2.avi and so on
+TMPDIR=`mktemp -d /tmp/tvplay.XXXXXXXX`
+COUNT=0
+while [ -n "$1" ]
+do
+    COUNT=`expr $COUNT + 1`
+    ln -s "$1"  $TMPDIR/$COUNT.avi
+    VIDS="$VIDS $TMPDIR/$COUNT.avi"
+    shift
+done
 
-echo "xterm -e \"$PLAYER $OPTS '$VIDS'\"" > $TVPLAY_XINITRC
+echo "xterm -e \"$PLAYER $OPTS $VIDS\"" > $TVPLAY_XINITRC
 XINITRC=$TVPLAY_XINITRC xinit
+
+# Now delete xinitrc and links
+rm -r $TMPDIR
 rm $TVPLAY_XINITRC
