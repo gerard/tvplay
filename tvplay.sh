cb2c2c1a926cbbb932bcfaf04e5d2f1d093d55f2
Gerard Lledo <gerard.lledo@gmail.com>
2010-02-09 21:58:18 +0200

    Add getopts to list the history and get help

--- tvplay.sh
+++ tvplay.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
 TVPLAY_XINITRC=$HOME/.tvplay.xinitrc
+TVPLAY_HISTORY=$HOME/.tvplay.history
 PLAYER=mplayer
 OPTS="-fs"
+
+while getopts "lh" FLAG
+do
+    case $FLAG in
+    l)
+        less $TVPLAY_HISTORY
+        exit 0
+        break;;
+    h)
+        echo "Sorry, nothing is going to help you.  Here is the code instead:"
+        cat $0
+        exit 0
+        break;;
+    esac
+done
 
 # Since using filenames with special characters is a PITA, we just link the
 # the file instead using a symbolic link.  That way we don't have to deal with
@@ -14,7 +30,7 @@ while [ -n "$1" ]
 do
     COUNT=`expr $COUNT + 1`
     ln -s "$1"  $TMPDIR/$COUNT.avi
-    echo `date` "$1" >> ~/.tvplay.history
+    echo `date` "$1" >> $TVPLAY_HISTORY
     VIDS="$VIDS $TMPDIR/$COUNT.avi"
     shift
 done
